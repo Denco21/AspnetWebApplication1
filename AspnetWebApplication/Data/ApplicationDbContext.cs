fca4f1cd4d013de4a37a6caef6d052c1b1d68181
Denco21 <90768523+Denco21@users.noreply.github.com>
2023-03-08 15:51:23 +0100

    Add project files.

---
+++ AspnetWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspnetWebApplication.Models;
+
+namespace AspnetWebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AspnetWebApplication.Models.Joke> Joke { get; set; }
+    }
+}
